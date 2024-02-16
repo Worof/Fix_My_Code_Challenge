c04ce4639265fcec64e534fc2902bf88b726bafd
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-02-16 21:01:41 +0200

    Print square

---
+++ 0x00-challenge/3-user.py
@@ -0,0 +1,58 @@
+import hashlib
+import uuid
+
+class User():
+    """
+    User class:
+    - id: public string unique (uuid)
+    - password: private string hash in MD5
+    """
+
+    __password = None
+
+    def __init__(self):
+        """
+        Initialize a new user:
+        - assigned a unique `id`
+        """
+        self.id = str(uuid.uuid4())
+
+    @property
+    def password(self):
+        """
+        Password getter
+        """
+        return self.__password
+
+    @password.setter
+    def password(self, pwd):
+        """
+        Password setter:
+        - `None` if `pwd` is `None`
+        - `None` if `pwd` is not a string
+        - Hash `pwd` in MD5 before assign to `__password`
+        """
+        if pwd is None or not isinstance(pwd, str):
+            self.__password = None
+        else:
+            # Correctly store the hashed password in the private variable
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
+
+    def is_valid_password(self, pwd):
+        """
+        Valid password:
+        - `False` if `pwd` is `None`
+        - `False` if `pwd` is not a string
+        - `False` if `__password` is `None`
+        - Compare `__password` and the MD5 value of `pwd`
+        """
+        if pwd is None or not isinstance(pwd, str):
+            return False
+        if self.__password is None:
+            return False
+        # Ensure the comparison is case-insensitive
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
+
+if __name__ == '__main__':
+    print("Test User")
+    # Your test code remains the same
